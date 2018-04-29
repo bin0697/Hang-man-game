8c94912cd9418648f50122b7546ca5bad708f0e8
Quoc Son <bin0697@gmail.com>
2018-04-29 22:27:07 +0700

    1st commit

---
+++ client.py
@@ -0,0 +1,112 @@
+import socket
+import os
+import sys
+import thread
+import time
+import json
+import re
+
+server_ip = "localhost"
+server_port = 5020
+username = raw_input ("Enter username: ")
+turn = -1
+appe = 0
+mess = ''
+scroce = 0
+
+color = 0
+allow_color = True
+
+try:
+    color = sys.argv[4]
+    if color == "--nocolor":
+        allow_color = False
+        color = "0"
+except: pass
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def get_messages():
+    global sock, username, color, mess, turn, appe, scroce, game
+    while True:
+        data = None
+        try:
+            data, addr = sock.recvfrom(1024, socket.MSG_DONTWAIT)
+        except socket.error:
+            time.sleep(0.01)
+        
+        if data:
+            try:
+                message = json.loads(data)
+                if (message['message'] != mess):
+                    words=zip(mess,message['message'])
+                    incorrect=len([c for c,d in words if c!=d])
+                    #print incorrect
+                    if (message['username'] == username):
+                        #mess = message['message']
+                        scroce += 100*incorrect
+                        print "scroce: ", scroce
+                        
+                    
+                    elif (message['username'] == "init"):
+                        mess = message['message']
+                        has_underscore = re.findall(r'_', mess)
+                        turn = len(has_underscore)
+                        print "You have ", turn, " turn"
+
+                    if (mess != ''):
+                        mess = message['message']
+                elif(message['message'] == mess):
+                    if(message['username'] == username):
+                        turn = turn - 1
+                        print "wrong, turn remaining: ", turn
+                        time.sleep(5)
+
+
+                print message['username'], ": " ,message['message']
+
+
+                if (message['message'] == username + " get the bonus point"):
+                        scroce += 500
+                        print "scroce: ", scroce
+
+                if (message['message'] == "Game over"):
+                        message = { "username" : username, "message" : "the scroce ", "scroce" : scroce  }
+                        sock.sendto( json.dumps(message), (server_ip, int(server_port)) )
+                
+                '''
+                if(message['username']):
+                    msg_str = message['username'] + ": " + message['message']
+                    
+                if len(message['message']) > 0:
+                    if allow_color:
+                        print "\033[%sm%s\033[0m" % (message['color'], msg_str)
+                '''
+
+            except ValueError:
+                print "error: tried to decode something invald"
+                
+def get_input():
+    global sock, username, color
+    try:
+        while True and turn != 0:
+            message = { "username" : username, "message" : raw_input().strip(), "color" : color  }
+            sock.sendto( json.dumps(message), (server_ip, int(server_port)) )
+    except KeyboardInterrupt:
+        print "byebye now"
+
+thread.start_new_thread(get_input, ())
+thread.start_new_thread(get_messages, ())
+
+message = { "username" : username, "message" : "/hello", "color" : color  }
+sock.sendto( json.dumps(message), (server_ip, int(server_port)) )
+message = { "username" : username, "message" : "/who", "color" : color  }
+sock.sendto( json.dumps(message), (server_ip, int(server_port)) )
+try: 
+    while 1:
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    print "bye"
+    message = { "username" : username, "message" : "/goodbye", "color" : color  }
+    sock.sendto( json.dumps(message), (server_ip, int(server_port)) )
+    sys.exit(0)
---
+++ server.py
@@ -0,0 +1,166 @@
+import socket
+import time
+import sys
+import json
+import time
+import random
+import re
+
+UDP_IP= "localhost"
+UDP_PORT=5020
+guesses = ''
+game = 0
+
+rand_line = random.choice(open('words.txt').readlines())
+
+reg = re.compile('(.+)(\t)(.+)')
+if reg.search(rand_line):
+    word = reg.search(rand_line).group(1)
+    desc = reg.search(rand_line).group(3)
+
+print('Description: ' + desc)
+
+sock = socket.socket( socket.AF_INET, 
+                      socket.SOCK_DGRAM )
+
+sock.bind( (UDP_IP,UDP_PORT) )
+
+clients = []
+
+def broadcast(data):
+  global clients
+  for client in clients:
+    sock.sendto(data, client[1])
+
+while True:
+    data = None
+    message = None
+    # make a counter that starts with zero 
+    try:
+      
+      data, addr = sock.recvfrom( 1024, socket.MSG_DONTWAIT ) 
+
+      message = json.loads(data)
+      client = (message['username'], addr)
+      if client not in clients:
+        clients.append(client)
+      
+    except socket.error:
+
+      time.sleep(0.01)
+
+    if data:           
+      print data
+      try: 
+
+        if( message['message'].startswith("/hello") ):
+
+          outjson = {"username" : "server",\
+            "message" : message['username'] + " joined the chat",\
+            "color" : 35 } 
+          broadcast( json.dumps(outjson) )
+
+          if (game == 0):
+
+            outjson = {"username" : "server",\
+              "message" : "type start to begin ",\
+              "color" : 35 } 
+            broadcast( json.dumps(outjson) )
+
+          elif (game == 1):
+
+            outjson = {"username" : message['username'],\
+              "message" : "game in process, plz wait ",\
+              "color" : 35 } 
+            broadcast( json.dumps(outjson) )
+
+        elif ( message['message'].startswith("/who") ):
+
+          outjson = {"username" : "server",\
+            "message" : "people in room: " + ', '.join([y[0] for y in clients]),\
+            "color" : 35 } 
+
+          sock.sendto( json.dumps(outjson), client[1] )
+
+        elif ( message['message'].startswith("/goodbye") ):
+
+          outjson = {"username" : "server",\
+            "message" : message['username'] + " left the chat",\
+            "color" : 35 } 
+
+          clients.remove(client)
+
+          broadcast( json.dumps(outjson) )
+
+        elif ( message['message'].startswith("/me") ):
+
+          newmsg = message['username'] + message['message'][3:]
+          outjson = {"username" : None,\
+            "message" : newmsg,\
+            "color" : message['color'] } 
+
+          broadcast( json.dumps(outjson) )
+  
+        elif (message['message'] == "start" and game == 0):
+          game = 1
+          ans = ''
+          for char in word: 
+            if char == " ":
+                   ans = ans + "  "
+            elif char.lower() not in guesses and char != " ":
+              # if not found, print a dash
+                   ans = ans + " _" 
+
+          outjson = {"username" : "server",\
+            "message" : desc,\
+            "color" : message['color'] }
+          broadcast( json.dumps(outjson) )
+
+          outjson = {"username" : "init",\
+            "message" : ans,\
+            "color" : message['color'] }
+          broadcast( json.dumps(outjson) )
+
+        elif (len(message['message']) == 1):
+          guesses = guesses + message['message']
+          ans = ''
+          # for every character in secret_word    
+          for char in word:      
+          # see if the character is in the players guess
+              if char.lower() in guesses:    
+          
+              # print then out the character
+                   ans = ans + " " + char 
+
+              if char == " ":
+                   ans = ans + "  "
+              elif char.lower() not in guesses and char != " ":
+              # if not found, print a dash
+                   ans = ans + " _"  
+
+          has_underscore = re.findall(r' _', ans)  
+
+          outjson = {"username" : message['username'],\
+            "message" : ans,\
+            "color" : message['color'] } 
+          broadcast( json.dumps(outjson) )
+
+          if (len(has_underscore) == 0):
+
+            bonus = message['username'] + " get the bonus point"
+            outjson = {"username" : "server",\
+            "message" : bonus,\
+            "color" : message['color'] } 
+            broadcast( json.dumps(outjson) )
+
+            outjson = {"username" : "server",\
+            "message" : "Game over",\
+            "color" : message['color'] } 
+            broadcast( json.dumps(outjson) )
+
+        
+        print clients
+
+      except ValueError:
+
+        print "indecipherable json"
